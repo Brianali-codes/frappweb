552b483c9935677c6ab5ca9a62b3ac314795c79e
Brian <gamerfunky78@gmail.com>
2025-01-10 17:51:34 +0300

    update app title

--- src/home.jsx
+++ src/home.jsx
@@ -36,7 +36,7 @@ export default function Home(){
                     <div className='p-5 gap-5 flex flex-row justify-center items-center' id='navbar'>
                         <a href="https://ko-fi.com/brianalicodes"><img src='https://storage.ko-fi.com/cdn/kofi2.png?v=6%27%20border=%270%27' className='w-30 h-10 cursor-pointer' /></a>
                         <a href="/"><button className='text-white hover:text-cyan-400 kanit text-lg' >Download</button></a>
-                        <a href="https://github.com/brianali-codes/Frapp" target='_blank'><button className='text-white hover:text-cyan-400 kanit text-lg' >Links</button></a>
+                        <a href="https://brianali.netlify.app" target='_blank'><button className='text-white hover:text-cyan-400 kanit text-lg' >About the Developer</button></a>
                         <a href="https://github.com/brianali-codes/Frapp" target='_blank'><button className='text-white hover:text-cyan-400 kanit text-lg' >Features</button></a>
                         
                     
@@ -48,7 +48,7 @@ export default function Home(){
 
                         <a href="https://ko-fi.com/brianalicodes"><img src='https://storage.ko-fi.com/cdn/kofi2.png?v=6%27%20border=%270%27' className='w-30 h-10 cursor-pointer'/></a>
                         <a href="/"><button className='text-white hover:text-cyan-400 kanit text-lg'>Download</button></a>
-                        <a href="https://github.com/brianali-codes" target='_blank'><button className='text-white hover:text-cyan-400 kanit text-lg'>Links</button></a>
+                        <a href="https://brianali.netlify.app" target='_blank'><button className='text-white hover:text-cyan-400 kanit text-lg'>About the Developer</button></a>
                         <a href="https://github.com/brianali-codes" target='_blank'><button className='text-white hover:text-cyan-400 kanit text-lg'>Features</button></a>
                         
                     </div>
@@ -57,7 +57,7 @@ export default function Home(){
                 <br /><br />
                  <p className='playwrite text-white text-4xl text-center'>Frapp, Get video game giveaways Fast</p>
                  <br /><br />
-                 <p className='kanit text-white text-center'>Frapp is a mobile app that provides access to video game giveaways and free games. Users can participate in giveaways, download free games, and report bugs, all with seamless navigation, offering a user-friendly experience for discovering and enjoying free games on mobile devices.</p>
+                 <p className='kanit text-white text-center'>Frapp is a mobile app that alerts users when stores like Epic Games or Steam and much more offer paid games for free. It’s user-friendly, helping anyone discover and download free games effortlessly, while also allowing users to report bugs to improve the app’s functionality and overall experience.</p>
                 <br /><br />
                 <div className='flex flex-row justify-around items-center' id='home-sect'>
                     <motion.div
